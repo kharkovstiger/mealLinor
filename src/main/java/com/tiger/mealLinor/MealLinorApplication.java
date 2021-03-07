936eb81062a04a31394dfa858dc482331d9f9a89
Nick.Baru <nick.baru@ngsoft.com>
2021-03-07 15:49:13 +0200

    db

--- src/main/java/com/tiger/mealLinor/MealLinorApplication.java
+++ src/main/java/com/tiger/mealLinor/MealLinorApplication.java
@@ -1,9 +1,12 @@
 package com.tiger.mealLinor;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 public class MealLinorApplication {
 
     public static void main(String[] args) {
