6f8d861e6e6a0206934900abb41817d18cf85c6c
Nick.Baru <nick.baru@ngsoft.com>
2021-03-10 16:47:15 +0200

    get meals in 10% fork

--- src/main/java/com/tiger/mealLinor/repository/CrudMealRepository.java
+++ src/main/java/com/tiger/mealLinor/repository/CrudMealRepository.java
@@ -17,5 +17,5 @@ public interface CrudMealRepository extends MongoRepository<Meal, String> {
     @Query(value = "{}")
     List<Meal> find(Double calories, Double protein, Double fats, Double carbs);
 
-    List<Meal> findByCaloriesGreaterThanEqualAndCaloriesLessThanEqualAndProteinGreaterThanEqualAAndProteinLessThanEqualAndFatsGreaterThanEqualAndFatsLessThanEqualAndCarbsGreaterThanEqualAndCarbsLessThanEqual(Double caloriesMin, Double caloriesMax, Double proteinMin, Double proteinMax, Double fatsMin, Double fatsMax, Double carbsMin, Double carbsMax);
+    List<Meal> findByCaloriesGreaterThanEqualAndCaloriesLessThanEqualAndProteinGreaterThanEqualAndProteinLessThanEqualAndFatsGreaterThanEqualAndFatsLessThanEqualAndCarbsGreaterThanEqualAndCarbsLessThanEqual(Double caloriesMin, Double caloriesMax, Double proteinMin, Double proteinMax, Double fatsMin, Double fatsMax, Double carbsMin, Double carbsMax);
 }
